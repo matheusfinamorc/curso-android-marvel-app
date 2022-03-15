f528899150287f439b2b5505c541323100f078a2
matheus.finamor <matheus.finamor@compasso.com.br>
2022-03-15 16:00:19 -0300

    Implementa testes unitários

---
+++ app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.paging.PagingData
+import com.example.core.domain.model.Character
+import com.example.core.usecase.GetCharactersUseCase
+import com.example.testing.MainCoroutineRule
+import com.example.testing.model.CharacterFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@ExperimentalCoroutinesApi // coloca tudo na mesma thread
+@RunWith(MockitoJUnitRunner::class) // define quem vai executar o teste
+class CharactersViewModelTest {
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    // sempre instanciar a classe que esta testando
+    private lateinit var charactersViewModel: CharactersViewModel
+
+    private val charactersFactory = CharacterFactory()
+
+    private val pagingDataCharacters = PagingData.from(
+        listOf(
+            charactersFactory.create(CharacterFactory.Hero.ThreeDMan),
+            charactersFactory.create(CharacterFactory.Hero.ABomb)
+        )
+    )
+
+    @Before //faz com que esta funcao seja chamada antes de cada funcao de teste ser executada
+    fun setUp() {
+        charactersViewModel = CharactersViewModel(getCharactersUseCase)
+    }
+
+    @Test
+    fun `should validate the paging data object values when calling charactersPagingData`() =
+        // trabalhe com as fun de coroutines dentro do runBlockingTest
+        runBlockingTest {
+            whenever(
+                getCharactersUseCase.invoke(any())
+            ).thenReturn(
+                flowOf(
+                    pagingDataCharacters
+                )
+            )
+
+            val result = charactersViewModel.charactersPagingData("")
+
+            // verifica quantos resultados terá no flow
+            assertEquals(1, result.count())
+
+        }
+
+    @Test(expected = RuntimeException::class)
+    fun `should throw an exception when the calling to the use case returns an exception`(){
+        runBlockingTest {
+            whenever(getCharactersUseCase.invoke(any()))
+                .thenThrow(RuntimeException())
+
+            charactersViewModel.charactersPagingData("")
+        }
+    }
+}
